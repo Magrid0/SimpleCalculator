65c86d5ab4ff89ee1e8aa04942e2b01a0e615810
Magrid <73496008+Magrid0@users.noreply.github.com>
2023-05-18 16:59:15 +0200

    Update SimpleCalculator.java

--- SimpleCalculator.java
+++ SimpleCalculator.java
@@ -1,24 +1,66 @@
 import java.util.Scanner;
 
+class GlobalVariables {
+	public static int a, b; // Declare global variables.
+}
+
+class Calculator {
+
+	public static void sum() {
+
+		// Sum the two numbers.
+		int sum = GlobalVariables.a + GlobalVariables.b;
+		System.out.println("\nThe result is: " + sum);
+
+	}
+
+	public static void subtraction() {
+
+		// Subtract the two numbers.
+		int subtract = GlobalVariables.a - GlobalVariables.b;
+		System.out.println("\nThe result is: " + subtract);
+
+	}
+
+	public static void multiplication() {
+
+		// Multiply the two numbers.
+		int multiply = GlobalVariables.a * GlobalVariables.b;
+		System.out.println("\nThe result is: " + multiply);
+
+	}
+
+	public static void division() {
+
+		if (GlobalVariables.b != 0) {
+			// Divide the two numbers.
+			int divide = GlobalVariables.a / GlobalVariables.b;
+			System.out.println("\nThe result is: " + divide);
+		} else {
+			System.out.println("\nCannot divide by zero.");
+		}
+
+	}
+
+}
+
 public class SimpleCalculator {
 
 	public static void main(String[] args) {
 		
 		// Welcome message.
-		System.out.println("Simple Calculator by Magrid0");
+		System.out.println("Simple Calculator by Magrid0\n");
 		
 		// Initialize scanner.
 		Scanner scanner = new Scanner(System.in);
 		
 		// Enter the first number.
-		System.out.print("Enter the firt number: ");
-		String StringA = scanner.nextLine();
-		int a = Integer.parseInt(StringA);
+		System.out.print("Enter the first number: ");
+		GlobalVariables.a = Integer.parseInt(scanner.nextLine());
 		
 		// Enter the second number.
 		System.out.print("Enter the second number: ");
-		String StringB = scanner.nextLine();
-		int b = Integer.parseInt(StringB);
+		GlobalVariables.b = Integer.parseInt(scanner.nextLine());
 		
 		// Choose sum or subtraction.
 		System.out.println("Do you want to Sum or Subtract these two numbers?");
@@ -26,40 +68,19 @@ public class SimpleCalculator {
 		System.out.println("[2] Subtract");
 		System.out.println("[3] Multiply");
 		System.out.println("[4] Divide");
-		String StringChoice = scanner.nextLine();
-		int choice = Integer.parseInt(StringChoice);
-		
-		if (choice == 1) {
-			
-			// Sum the two numbers.
-			int sum = a + b;
-			System.out.println(sum);
-			
-		} else if (choice == 2) {
-			
-			// Subtract the two numbers.
-			int subtract = a - b;
-			System.out.println(subtract);
-			
-		} else if (choice == 3) {
-			
-			// Multiply the two numbers.
-			int multiply = a * b;
-			System.out.println(multiply);
-			
-		} else if (choice == 4) {
-			
-			// Divide the two numbers.
-			int divide = a/b;
-			System.out.println(divide);
-			
-		} else {
-			
-			// Invalid choice message.
-			System.out.println("Invalid choice, select 1 or 2.");
-			
+		int choice = Integer.parseInt(scanner.nextLine());
+
+		switch (choice) {
+
+			case 1 -> Calculator.sum();
+
+			case 2 -> Calculator.subtraction();
+
+			case 3 -> Calculator.multiplication();
+
+			case 4 -> Calculator.division();
 		}
-		
+
 		// Prompt the user to exit the program.
 		System.out.println("\nPress ENTER to exit...");
 		scanner.nextLine();
